c0e7db9f78762c4b4a9f57b175975131fda56e83
Arun <arunjayas123@gmail.com>
2023-03-12 20:32:13 +0530

    First commit in the March 23

---
+++ src/main/java/GitProject1/GItProject1/App.java
@@ -0,0 +1,13 @@
+package GitProject1.GItProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
